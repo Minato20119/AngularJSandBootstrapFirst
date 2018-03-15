943b9fb7ae1e9cf7ac29d6946d5e43b6afdb6ff0
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2018-03-15 12:11:12 +0700

    Add files via upload

---
+++ AssignmentNumber1REST/src/main/java/com/minato/springboot/controller/AppController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.minato.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Minato
+ *
+ */
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	String home(ModelMap modal) {
+		modal.addAttribute("title", "CRUD Example");
+		return "index";
+	}
+
+	@RequestMapping("/partials/{page}")
+	String partialHandler(@PathVariable("page") final String page) {
+		return page;
+	}
+
+}
